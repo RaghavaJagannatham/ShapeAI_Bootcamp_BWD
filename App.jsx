fe629e083e0f6bc31c07c9972a9fc871203d95a6
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2021-06-05 14:17:12 +0530

    ShapeAI_BWD

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Block from "./Block";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Block />
+      <Block />
+      <Block />
+      <Footer />
+    </div>
+  );
+}
+export default App;
